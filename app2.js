358e088b15c1fc48acc8c5675006518f466a871e
Victor-Valery <81595723+Victor-Valery@users.noreply.github.com>
2021-06-16 21:53:13 +0200

    js_studies

---
+++ app2.js
@@ -0,0 +1,38 @@
+// console.time();
+// console.log('Hello World');
+// console.info('some infor');
+// console.warn('some warning');
+// console.error('error !!!');
+// ------------------------------
+// console.log('_________')
+// var myName = 'Victor';
+// console.log(myName);
+// myName = 'Valery';
+// console.log(myName);
+// ------------------------------
+const PI = 3.141521; 
+// // degismeyen sabit bir deger atamak icin "const" kullanilir
+
+let myNum = 5;
+// // degisebilen bir deger atamak icin "let" kullanilir
+
+console.log(PI);
+
+myNum = 8;
+
+console.log(myNum);
+
+ alert('Hallo');
+
+ var country = prompt('Enter your country: ');
+
+ console-log('Your country is', country);
+
+ var num1 = 42;
+
+ if (num1 == 42) {
+     let num2 = 22;
+     console.log(num1,num2);
+ }
+
+ 
